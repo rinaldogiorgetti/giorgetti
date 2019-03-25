677767fe69b0c4867f55dc2e30c44936f4d8be3f
rinaldogiorgetti <48737308+rinaldogiorgetti@users.noreply.github.com>
2019-03-26 00:33:00 +0100

    Update cmdfromanytext.lua

--- cmdfromanytext.lua
+++ cmdfromanytext.lua
@@ -1,9 +1,9 @@
-hexchat.register("rispondi", "2", "script bellissimo")
+hexchat.register("rispondi", "8", "script bellissimo")
 
 local function splayalert(word)
 	if word[2] == "!alert" then
-	hexchat.command ("say ALLERTA!!")
-	hexchat.command (splay "alert.wav")
+	hexchat.command("say ALLERTA!!")
+	hexchat.command("splay alert.wav")
 	else
 	return hexchat.EAT_NONE
 	end
