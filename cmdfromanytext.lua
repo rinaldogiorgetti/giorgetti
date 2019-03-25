fc8b721f08b3da2a115d1b93fadb9bb6077853a3
rinaldogiorgetti <48737308+rinaldogiorgetti@users.noreply.github.com>
2019-03-25 23:51:54 +0100

    Update cmdfromanytext.lua

--- cmdfromanytext.lua
+++ cmdfromanytext.lua
@@ -1,16 +1,16 @@
-hexchat.register("rispondi", "1", "script bellissimo")
-
-hexchat.hook_print("Channel Message", splayalert)
+hexchat.register("rispondi", "8", "script bellissimo")
 
 local function splayalert(word)
-
-	hexchat.print(word[2])
-	hexchat.print(word)
-	--if word ==!alert then
-	hexchat.command ("say cistoprovando")
-	--hexchat.command ("splay alert.wav")
-	--else
+	hexchat.print(word[2],"primo")
+	hexchat.command("say dico")
+	hexchat.command("say dico2")
+	hexchat.command("splay alert.wav")
+	if "word[2]" == "!alert" then
+	hexchat.command ("say dicobene")
+	hexchat.print(word[2], "sisi")
+	else
 	return hexchat.EAT_NONE
-	--end
+	end
 	end
 
+hexchat.hook_print("Channel Message", splayalert)
